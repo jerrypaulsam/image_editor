cfd4afc1cf6f0ed4dc1f7acbf52f3687c33db646
jerrypaulsam <62291043+jerrypaulsam@users.noreply.github.com>
2023-07-15 12:55:56 +0530

    Update image_editor_plus.dart

--- lib/image_editor_plus.dart
+++ lib/image_editor_plus.dart
@@ -243,12 +243,12 @@ class _MultiImageEditorState extends State<MultiImageEditor> {
           ],
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Text("Click on images to edit.", style: Theme.of(context).textTheme.bodyLarge!.copyWith(
                 fontWeight: FontWeight.w800, color: Colors.white
             ), textAlign: TextAlign.center,),
-            
+
             SizedBox(
               height: 332,
               width: double.infinity,
