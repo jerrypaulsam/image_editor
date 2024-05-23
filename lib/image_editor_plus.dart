8d107008dd50c4d13e671765b055dbf6b34fde28
jerrypaulsam <62291043+jerrypaulsam@users.noreply.github.com>
2024-05-23 12:57:34 +0530

    Update image_editor_plus.dart

--- lib/image_editor_plus.dart
+++ lib/image_editor_plus.dart
@@ -8,7 +8,7 @@ import 'package:colorfilter_generator/presets.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+// import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hand_signature/signature.dart';
 import 'package:image/image.dart' as img;
